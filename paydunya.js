d58116a514dd494f75b1d4f9042b8b1d7b3ce220
OMORES MAC <fallsouleymane4k@gmail.com>
2023-11-10 14:04:14 +0000

    Ajouter les credentiel paydunya api : put it later on .evn

---
+++ paydunya.js
@@ -0,0 +1,10 @@
+
+var paydunya = require('paydunya');
+
+var setup = new paydunya.Setup({
+  masterKey: 'po3uNTDv-wyVm-Vw5G-2Y11-jDQKoKzDRmyZ',
+  privateKey: 'test_private_zs0y0grEJV8UPpZp5h7qtjK7UVk',
+  publicKey: 'test_public_IYiIeTC9LNHKwItliYGjMrt6PgG',
+  token: 'WAg0swUiSgJM85Yc18eL',
+  mode: 'test' // Optionnel. Utilisez cette option pour les paiements tests.
+});
